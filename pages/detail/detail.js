4358e595adf2a639173b1f5215c7ad5f098257c0
bcy <quanshijiezuis@sina.com>
2020-04-15 01:34:08 +0800

    20201415-发散功能：详情页轮播图只显示最多五张(预览也是最多五张)

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,6 +1,7 @@
 // pages/detail/detail.js
 /* jshint esversion:6 */
 const fetch = require('../../utils/fetch');
+// const util=require('../../utils/com.wxs');
 Page({
 
   /**
@@ -9,13 +10,19 @@ Page({
   data: {
     shop: {}
   },
+  // 处理数组：如果数组长度>5,截取前5个元素组成的数组。如果数组长度<=5,截取索引0,……arr.length-1 这些元素组成的数组
+  filterHandle: function (arr) {
+    return arr.filter((item, index, array) => {
+      return array.length > 5 ? (index < 5) : index < arr.length;
+    })
+  },
   // 点击轮播图中任意一张图片，开启【图片预览】
   previewHandle: function (e) {
     // 测试代码-测试传入的当前图片url,e.target.dataset.src拿到当前图片url
     console.log(e);
     wx.previewImage({
       current: e.target.dataset.src, // 当前显示图片的链接，不填则默认为 urls 的第一张
-      urls: this.data.shop.images
+      urls: this.filterHandle(this.data.shop.images)
     });
   },
   /**
@@ -24,7 +31,7 @@ Page({
   onLoad: function (options) {
     // 测试代码：测试商铺列表传递的id值是否成功？
     console.log(options);
-    fetch(`shops/${options.id}`).then(res => {
+    fetch(`shops/${options.item}`).then(res => {
       // 测试代码
       console.log(res);
       this.setData({
